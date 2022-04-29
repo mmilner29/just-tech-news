934b16a5c5d178c15190124c4e18fc0d0d0a6b52
mmilner29 <madisonm4514@gmail.com>
2022-04-29 10:06:25 -0700

    login/logout

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,41 +1,55 @@
-const sequelize = require('../config/connection');
-const { Post, User, Comment } = require('../models');
+const sequelize = require("../config/connection");
+const { Post, User, Comment } = require("../models");
 
-const router = require('express').Router();
+const router = require("express").Router();
 
-router.get('/', (req, res) => {
-    Post.findAll({
-      attributes: [
-        'id',
-        'post_url',
-        'title',
-        'created_at',
-        [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+router.get("/", (req, res) => {
+  console.log(req.session);
+  Post.findAll({
+    attributes: [
+      "id",
+      "post_url",
+      "title",
+      "created_at",
+      [
+        sequelize.literal(
+          "(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)"
+        ),
+        "vote_count",
       ],
-      include: [
-        {
-          model: Comment,
-          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-          include: {
-            model: User,
-            attributes: ['username']
-          }
-        },
-        {
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
           model: User,
-          attributes: ['username']
-        }
-      ]
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      const posts = dbPostData.map((post) => post.get({ plain: true }));
+      // console.log(dbPostData[0]);
+      res.render("homepage", { posts });
     })
-      .then(dbPostData => {
-        const posts = dbPostData.map(post => post.get({ plain: true }));
-        // console.log(dbPostData[0]);
-        res.render('homepage', { posts });
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  });
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+  res.render("login");
+});
 
 module.exports = router;
